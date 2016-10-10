623556ebd94906f1b0b954541431ead54372458f
James Cook <jamescook.106@gmail.com>
2016-10-10 18:17:40 +0100

    Added a title bar and some text for the index page

--- GRChombo/config/routes.rb
+++ GRChombo/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  get "/:page" => "welcome#show"
+  get "/:page" => "welcome#index"
 
-  root 'welcome#show', page: "index"
+  root 'welcome#index', page: "index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
