3972fd7adccd0b456981917129a17bc65a8f8047
James Cook <jamescook.106@gmail.com>
2016-10-10 21:56:42 +0100

    test database and basic routing tests

--- GRChombo/db/schema.rb
+++ GRChombo/db/schema.rb
@@ -11,75 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160919174511) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "impressions", force: :cascade do |t|
-    t.string   "impressionable_type"
-    t.integer  "impressionable_id"
-    t.integer  "user_id"
-    t.string   "controller_name"
-    t.string   "action_name"
-    t.string   "view_name"
-    t.string   "request_hash"
-    t.string   "ip_address"
-    t.string   "session_hash"
-    t.text     "message"
-    t.text     "referrer"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "impressions", ["controller_name", "action_name", "ip_address"], name: "controlleraction_ip_index", using: :btree
-  add_index "impressions", ["controller_name", "action_name", "request_hash"], name: "controlleraction_request_index", using: :btree
-  add_index "impressions", ["controller_name", "action_name", "session_hash"], name: "controlleraction_session_index", using: :btree
-  add_index "impressions", ["impressionable_type", "impressionable_id", "ip_address"], name: "poly_ip_index", using: :btree
-  add_index "impressions", ["impressionable_type", "impressionable_id", "request_hash"], name: "poly_request_index", using: :btree
-  add_index "impressions", ["impressionable_type", "impressionable_id", "session_hash"], name: "poly_session_index", using: :btree
-  add_index "impressions", ["impressionable_type", "message", "impressionable_id"], name: "impressionable_type_message_index", using: :btree
-  add_index "impressions", ["user_id"], name: "index_impressions_on_user_id", using: :btree
-
-  create_table "jobs", force: :cascade do |t|
-    t.text     "name"
-    t.boolean  "primary"
-    t.boolean  "secondary"
-    t.text     "url"
-    t.text     "tagline"
-    t.text     "short"
-    t.text     "content"
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
-    t.string   "photo_file_name"
-    t.string   "photo_content_type"
-    t.integer  "photo_file_size"
-    t.datetime "photo_updated_at"
-    t.datetime "expiry"
-    t.datetime "release"
-    t.integer  "impressions_count",  default: 0
-    t.text     "contact"
-    t.text     "copyright"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
-    t.boolean  "admin",                  default: false
-    t.boolean  "sadmin",                 default: false
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
--- GRChombo/test/controllers/welcome_controller_test.rb
+++ GRChombo/test/controllers/welcome_controller_test.rb
@@ -2,7 +2,19 @@ require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
   test "should get index" do
-    get :index
+    get :index, page: 'index'
+    assert_response :success
+  end
+  test "should get movies" do
+    get :index, page: 'movies'
+    assert_response :success
+  end
+  test "should get publications" do
+    get :index, page: 'publications'
+    assert_response :success
+  end
+  test "should get collaborators" do
+    get :index, page: 'collaborators'
     assert_response :success
   end
 
